b7e099fbf8889435d7992deaa3e2fb350ab0aca8
nc133878 <crawford.neal18@gmail.com>
2025-12-17 18:19:26 -0500

    Updates to fix mobile bugs

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,49 +3,49 @@ import { nanoid } from 'nanoid'
 
 function Question(props) {
 
-  const labelArray = props.allAnswers[props.index].map((answer ,index) => {
-    let styles = {backgroundColor: 'none'}
+  const labelArray = props.allAnswers[props.index].map((answer, index) => {
+    const inputId = `${props.questionID}-${index}`
+
+    let styles = {}
     if (props.isSubmitted) {
-      console.log("Q"+ index + ": " + props.selectedAnswer[props.questionID])
-      console.log("C"+ index + ": " + props.correctAnswers[index])
-      if (props.correctSelections.includes(answer) && props.selectedAnswer[props.questionID] === answer) {
-        styles = {
-          backgroundColor: '#94D7A2',
-          border: 'none'
-        }
-      }else if (props.incorrectSelections.includes(answer) && props.selectedAnswer[props.questionID] === answer) {
-        styles = { 
-          backgroundColor: '#F8BCBC', 
-          opacity: 0.5,
-          border: 'none'
-        }
+      if (
+        props.correctSelections.includes(answer) &&
+        props.selectedAnswer[props.questionID] === answer
+      ) {
+        styles = { backgroundColor: '#94D7A2', border: 'none' }
+      } else if (
+        props.incorrectSelections.includes(answer) &&
+        props.selectedAnswer[props.questionID] === answer
+      ) {
+        styles = { backgroundColor: '#F8BCBC', opacity: 0.5, border: 'none' }
       }
-      }
-    return(
-      <div key={nanoid()} className='radio-buttons' >
-        <input 
+    }
+
+    return (
+      <div key={nanoid()} className="radio-buttons">
+        <input
           type="radio"
-          id={answer}
+          id={inputId}
           name={props.questionID}
           value={answer}
           onChange={props.handleChange}
           checked={props.selectedAnswer[props.questionID] === answer}
         />
-      <label style={ styles }>
-            {answer}
+        <label htmlFor={inputId} style={styles}>
+          {answer}
         </label>
       </div>
-      )
-    })
+    )
+  })
 
   return (
-        <div className='question-container'>
-                <h3 key={nanoid()}>{props.decode(props.detail.question)}</h3>
-                <form key={nanoid()} className='answer-container'>                    
-                    {labelArray}
-                </form>
-                <hr />
-        </div>
+    <div className="question-block">
+      <h3>{props.decode(props.detail.question)}</h3>
+      <form className="answer-container">
+        {labelArray}
+      </form>
+      <hr />
+    </div>
   )
 }
 
